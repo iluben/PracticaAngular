1441046a843d5280f1ce79b2acae795f34325259
Jose Antonio Heredia del Toro <jaheredia@vectoritcgroup.com>
2018-05-02 13:46:23 +0200

    Add forms

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from './menu/menu.component';
 import { CardComponent } from './card/card.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   declarations: [MenuComponent, CardComponent],
   exports: [MenuComponent, CardComponent]
 })
--- src/app/paginas/paginas.module.ts
+++ src/app/paginas/paginas.module.ts
@@ -4,11 +4,23 @@ import { CursosComponent } from './cursos/cursos.component';
 import { RouterModule, Routes } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { ComponentsModule } from '../components/components.module';
+import { NuevoComponent } from './nuevo/nuevo.component';
+import { FormsModule } from '@angular/forms';
 
-export const ROUTES: Routes = [{ path: '', component: CursosComponent }];
+export const ROUTES: Routes = [
+  { path: '', component: CursosComponent },
+  { path: 'nuevo', component: NuevoComponent }
+];
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(ROUTES), ComponentsModule],
-  declarations: [CursosComponent]
+  imports: [
+    CommonModule,
+    RouterModule.forChild(ROUTES),
+    ComponentsModule,
+    FormsModule
+  ],
+  declarations: [
+    CursosComponent,
+    NuevoComponent]
 })
 export class PaginasModule {}
